8abb2416fd5dadb8fc9b0f49f153a21b30bd3b35
ZacHaque <36503712+ZacHaque@users.noreply.github.com>
2018-11-12 01:06:51 +1100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,22 +9,25 @@ sudo apt  install ruby-bundler -y
 
 
 # make change on nginx config to forward traffic
+# location / {
+#    proxy_pass       http://localhost:4567;
+#    proxy_set_header Host      $host;
+#    proxy_set_header X-Real-IP $remote_addr;
+#}
 
 
-sudo vim /etc/nginx/sites-enabled/default 
-
-
-location / {
-    proxy_pass       http://localhost:4567;
-    proxy_set_header Host      $host;
-    proxy_set_header X-Real-IP $remote_addr;
-}
+#sudo vim /etc/nginx/sites-enabled/default 
+sudo git clone  https://github.com/ZacHaque/sinatraproject/
+sudo cp sinatraproject/default /etc/nginx/sites-enabled/
+sudo chmod 777 /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
 
-mkdir sinatraproject
-git clone https://github.com/rea-cruitment/simple-sinatra-app.git
-cd simple-sinatra-app
+
+#git clone https://github.com/rea-cruitment/simple-sinatra-app.git
+#cd simple-sinatra-app
+
+
 
 bundle install
 bundle exec rackup
